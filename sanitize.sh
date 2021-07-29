8d3fa295b5ab234896aea537c5036bb1a6e5ccf5
Alex Shaffer <michael-byrne.shaffer@sogeti.com>
2021-07-29 11:07:32 +0200

    Fix sanitize script to use correct paths for Ragnarok.

--- sanitize.sh
+++ sanitize.sh
@@ -10,6 +10,6 @@ echo "Redacting passwords for TheIsland..."
 sed "s/Password=.*/Password=_REDACTED_/g" private-src/TheIsland/GameUserSettings.ini > TheIsland/GameUserSettings.ini
 sed "s/Password=.*/Password=_REDACTED_/g" private-src/TheIsland/Game.ini > TheIsland/Game.ini 
 echo "Redacting passwords for Ragnarok..."
-sed "s/Password=.*/Password=_REDACTED_/g" private-src/TheIsland/GameUserSettings.ini > Ragnarok/GameUserSettings.ini
-sed "s/Password=.*/Password=_REDACTED_/g" private-src/TheIsland/Game.ini > Ragnarok/Game.ini 
+sed "s/Password=.*/Password=_REDACTED_/g" private-src/Ragnarok/GameUserSettings.ini > Ragnarok/GameUserSettings.ini
+sed "s/Password=.*/Password=_REDACTED_/g" private-src/Ragnarok/Game.ini > Ragnarok/Game.ini 
 echo "Sanitized config files created."
